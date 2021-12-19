d54633aaf45959c6188ce9273b0103967a2a071e
inoshiro <inoshirou@gmail.com>
2021-12-19 03:26:41 +0000

    init project

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header"><h1 class="site-title">いぬいのうた</h1></div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+.header {
+  height: 50px;
+  background-color: rgb(187, 157, 161);
+}
+
+.site-title {
+  margin: 0;
+  font-family: 'Zen Kurenaido';
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+<Header />
+</template>
+
+<script>
+export default {}
+</script>
